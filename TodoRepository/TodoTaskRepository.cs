abed1b1e892b933d47e326ba28a7f24bb7687c79
Jonathon <Jonathon@DESKTOP-1UECS49>
2025-02-07 15:27:42 -0500

    Proper Staging of Application for Future work

---
+++ TodoRepository/TodoTaskRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using TodoService;
+using Microsoft.Data.SqlClient;
+
+namespace TodoRepository
+{
+    public class TodoTaskRepository
+    {
+        private string ConnectionString = string.Empty;
+        public TodoTaskRepository(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public List<TodoTask> Select()
+        {
+            List<TodoTask> items = new List<TodoTask>();
+
+            using (var sqlConnection = new SqlConnection(ConnectionString))
+            {
+                sqlConnection.Open();
+                using (var sqlCommand = new SqlCommand("[dbo].[SelectAllItems]", sqlConnection))
+                {
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    using (SqlDataReader dataReader = sqlCommand.ExecuteReader())
+                    {
+                        if (dataReader.Read())
+                        {
+                            var item = new TodoTask
+                            {
+                                ItemID = (Int32)dataReader["UserID"],
+                                Name = dataReader["DisplayName"].ToString(),
+                                Completed = (bool)dataReader["Email"]
+                            };
+                            items.Add(item);
+                        }
+                    }
+                }
+            }
+
+            return items;
+        }
+    }
+}
---
+++ TodoService/TodoTask.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoService
+{
+    public class TodoTask
+    {
+        public int ItemID { get; set; }
+        public string? Name { get; set; }
+        public bool Completed { get; set; }
+    }
+}
